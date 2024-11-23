de477555195f2d7ee987ac9a92096f7be8bd70b0
Chetan Chandane <chetanchandane10@gmail.com>
2024-11-23 12:11:57 -0500

    add layout.html template file

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -123,4 +123,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.model
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
